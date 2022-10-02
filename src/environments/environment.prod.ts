9cecae5bb3e7a6fa9e216db14e053196e49a46e7
Sabhya Verma <sabhya.verma16@gmail.com>
2022-10-02 16:45:54 +0530

    Specify environment write keys

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  analytics: {
+    segmentWriteKey: 'PRODUCTION_SEGMENT_WRITE_KEY'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  analytics: {
+    segmentWriteKey: 'DEV_SEGMENT_WRITE_KEY'
+  }
 };
 
 /*
