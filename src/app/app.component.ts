bf379297ff430c3727ae28568fb78b317f803a8d
Sabhya Verma <sabhya.verma16@gmail.com>
2022-10-02 17:55:21 +0530

    Trigger page calls on router events

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { NavigationStart, Router } from '@angular/router';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'segment-analytics-with-angular';
+export class AppComponent implements OnInit, OnDestroy {
+  title = 'segment-analytics-with-angular'
+  routerEventSubscription: Subscription | undefined;
+
+  constructor(private _router: Router) {}
+
+  public ngOnInit(): void {
+    this.routerEventSubscription = this._router.events.subscribe((event) => {
+      if (event instanceof NavigationStart) {
+        analytics.page(event.url)
+      }
+    });
+  }
+
+  public ngOnDestroy(): void {
+    this.routerEventSubscription?.unsubscribe()
+  }
 }
